029ccd6b6bba32370b0e5cb2cfa03adac2f0f08e
wang <3080433063@qq.com>
2023-04-28 12:08:35 +0800

    push commit2

--- com/w/git/Test.java
+++ com/w/git/Test.java
@@ -4,8 +4,11 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
         System.out.println("push test");
     }
 }
