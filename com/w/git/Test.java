ccb906afa0156fe6ef3ddf23ef77a467852adecb
wang <3080433063@qq.com>
2023-04-28 11:21:25 +0800

    hot-fix commit2

--- com/w/git/Test.java
+++ com/w/git/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
