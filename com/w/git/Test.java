1c1b18790c5de343562e9f38e662081a4e8ef348
wang <3080433063@qq.com>
2023-04-29 12:03:45 +0800

    fot-fix commit5

--- com/w/git/Test.java
+++ com/w/git/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");      
         System.out.println("master test");
+        System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee test");
