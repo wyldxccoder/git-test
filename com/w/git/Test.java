d071eea051865c60e607e48957eab17ef1a26173
wyldxccoder <131867023+wyldxccoder@users.noreply.github.com>
2023-04-29 11:55:18 +0800

    Update Test.java

--- com/w/git/Test.java
+++ com/w/git/Test.java
@@ -2,10 +2,7 @@ package com.w.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello git");      
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
